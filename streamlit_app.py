5a5f652b3983c124b99e2300762f096d8288e57d
Jasmine <jasmine.s.jy.thompson@gmail.com>
2025-04-11 16:09:57 +0100

    Changed streamlit app to use the sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,22 @@
+import streamlit as st
 from analyzer import TokenSwapAnalyzer
 
 analyzer = TokenSwapAnalyzer()
 
-# Streamlit UI
+# Streamlit sidebar
+with st.sidebar:
+    st.markdown("Enter two token names (e.g., bitcoin, ethereum, etc...) and generate a risk report.")
+
+    from_token = st.text_input("Enter token we are converting from", value="bitcoin")
+    to_token = st.text_input("Enter token we are converting to", value="ethereum")
+
+    generate_report_button = st.button("Generate Report")
+
+# Streamlit main body
 st.title("🔍 DeFi Token Risk Analyzer")
-st.markdown("Enter two token names (e.g., bitcoin, ethereum, etc...) and generate a risk report.")
-
-from_token = st.text_input("Enter token we are converting from", value="bitcoin")
-to_token = st.text_input("Enter token we are converting to", value="ethereum")
-
 current_task = st.empty()
 
-if st.button("Generate Report"):
+if generate_report_button:
     current_task.text("Writing report...")
     report = analyzer.compare_tokens(from_token, to_token)  # BTC → ETH
     current_task.empty()
